be56a6d2da93ed6b4ca4f5b32b22a1db32554516
Henrik Mohr <henrik@meew.ee>
2015-02-11 13:51:58 +0100

    upgrade cljs 0.0-2760 -> 0.0.2816

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/clojurescript "0.0-2816"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.stuartsierra/component "0.2.2"]
                  [environ "1.0.0"]
