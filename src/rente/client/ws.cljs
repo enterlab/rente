3dd302b274c2a57ee0d52b02f5eab5a33b14319c
Henrik Mohr <henrik@meewee.com>
2015-03-25 23:01:44 +0100

    Enhanced the push event handler. Closing issue #4. Thanks to @tuhlmann!

--- src/rente/client/ws.cljs
+++ src/rente/client/ws.cljs
@@ -2,16 +2,18 @@
   (:require [taoensso.sente :as sente]
             [taoensso.sente.packers.transit :as sente-transit]))
 
+(defmulti push-msg-handler (fn [[id _]] id)) ; Dispatch on event key which is 1st elem in vector
+
+(defmethod push-msg-handler :rente/testevent
+  [[_ event]]
+  (js/console.log "PUSHed :rente/testevent from server: %s " (pr-str event)))
+
 (defmulti event-msg-handler :id) ; Dispatch on event-id
 ;; Wrap for logging, catching, etc.:
 
 (defmethod event-msg-handler :default ; Fallback
     [{:as ev-msg :keys [event]}]
     (js/console.log "Unhandled event: %s" (pr-str event)))
-
-(defmethod event-msg-handler :rente/testevent
-  [{:as ev-msg :keys [?data]}]
-    (js/console.log "Testevent received from server"))
 
 (defmethod event-msg-handler :chsk/state
   [{:as ev-msg :keys [?data]}]
@@ -21,10 +23,9 @@
 
 (defmethod event-msg-handler :chsk/recv
   [{:as ev-msg :keys [?data]}]
-  (js/console.log "Push event from server: %s" (pr-str ?data)))
+  (push-msg-handler ?data))
 
 (defn event-msg-handler* [{:as ev-msg :keys [id ?data event]}]
-  ;(js/console.log "Event: %s" (pr-str event))
   (event-msg-handler ev-msg))
 
 (let [packer (sente-transit/get-flexi-packer :edn)
@@ -39,7 +40,10 @@
 (sente/start-chsk-router! ch-chsk event-msg-handler*)
 
 (defn test-socket-callback []
-  (chsk-send! [:rente/testevent {:message "Hello socket Callback!"}] 2000 #(js/console.log "Callback received: " (pr-str %))))
+  (chsk-send!
+    [:rente/testevent {:message "Hello socket Callback!"}]
+    2000
+    #(js/console.log "CALLBACK from server: " (pr-str %))))
 
 (defn test-socket-event []
   (chsk-send! [:rente/testevent {:message "Hello socket Event!"}]))
