bdf29037abbf549ad8c9106c595b52b6f8b5ed87
Henrik Mohr <henrik@meewee.com>
2015-03-25 21:31:26 +0100

    min-lein-version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://enterlab.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/clojurescript "0.0-3149"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
