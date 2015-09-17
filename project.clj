9d8e7908e0ea0e418fc203af372c322c8329540f
Henrik Mohr <henrik@meewee.com>
2015-09-17 09:55:21 +0200

    clojurescript -> 1.7.122, environ -> 1.0.1, transit-cljs -> 0.8.225, reagent -> 0.5.1, lein-cljsbuild -> 1.1.0, figwheel -> 0.3.9, lein-figwheel -> 0.3.9, lein-environ -> 1.0.1

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.58"]
+                 [org.clojure/clojurescript "1.7.122"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.stuartsierra/component "0.2.3"]
-                 [environ "1.0.0"]
+                 [environ "1.0.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
 
@@ -19,12 +19,12 @@
 
                  [com.taoensso/sente "1.6.0" :exclusions [org.clojure/tools.reader]]
                  [com.cognitect/transit-clj "0.8.281" :exclusions [commons-codec]]
-                 [com.cognitect/transit-cljs "0.8.220"]
+                 [com.cognitect/transit-cljs "0.8.225"]
 
-                 [reagent "0.5.0"]
+                 [reagent "0.5.1"]
                  [org.webjars/bootstrap "3.3.5"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
 
   :source-paths ["src"]
   :resource-paths ["resources" "resources-index/prod"]
@@ -43,10 +43,10 @@
 
              :dev [:dev-config
                    {:dependencies [[org.clojure/tools.namespace "0.2.7"]
-                                   [figwheel "0.2.5"]]
+                                   [figwheel "0.3.9"]]
 
-                    :plugins [[lein-figwheel "0.2.5" :exclusions [org.clojure/tools.reader org.clojure/clojurescript clj-stacktrace]]
-                              [lein-environ "1.0.0"]]
+                    :plugins [[lein-figwheel "0.3.9" :exclusions [org.clojure/clojure org.clojure/clojurescript org.codehaus.plexus/plexus-utils]]
+                              [lein-environ "1.0.1"]]
 
                     :source-paths ["dev"]
                     :resource-paths ^:replace
