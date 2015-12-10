99417a18e9d4ae8d153fc00468ec8330abeccd9b
Henrik Mohr <luposlip@gmail.com>
2015-12-10 09:19:59 +0100

    UPGRADE TO LEININGEN 2.5.3! Updated cljs -> 1.7.189, cljsbuild -> 1.1.1, figwheel -> 0.5.0-2, core.async -> 0.2.374, transit.cljs -> 0.8.232, bootstrap -> 3.3.6, org.clojure/tools.namespace -> 0.2.10

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://enterlab.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.3"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
-                 [org.clojure/core.async "0.2.371"]
+                 [org.clojure/clojurescript "1.7.189"]
+                 [org.clojure/core.async "0.2.374"]
                  [com.stuartsierra/component "0.3.0"]
                  [environ "1.0.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
@@ -17,14 +17,14 @@
                  [compojure "1.4.0"]
                  [http-kit "2.1.19"]
 
-                 [com.taoensso/sente "1.6.0" :exclusions [org.clojure/tools.reader]]
-                 [com.cognitect/transit-clj "0.8.285" :exclusions [commons-codec]]
-                 [com.cognitect/transit-cljs "0.8.225"]
+                 [com.taoensso/sente "1.6.0"]
+                 [com.cognitect/transit-clj "0.8.285"]
+                 [com.cognitect/transit-cljs "0.8.232"]
 
                  [reagent "0.5.1"]
-                 [org.webjars/bootstrap "3.3.5"]]
+                 [org.webjars/bootstrap "3.3.6"]]
 
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.1"]]
 
   :source-paths ["src"]
   :resource-paths ["resources" "resources-index/prod"]
@@ -42,10 +42,10 @@
   :profiles {:dev-config {}
 
              :dev [:dev-config
-                   {:dependencies [[org.clojure/tools.namespace "0.2.7"]
-                                   [figwheel "0.3.9"]]
+                   {:dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                   [figwheel "0.5.0-2"]]
 
-                    :plugins [[lein-figwheel "0.3.9" :exclusions [org.clojure/clojure org.clojure/clojurescript org.codehaus.plexus/plexus-utils]]
+                    :plugins [[lein-figwheel "0.5.0-2"]
                               [lein-environ "1.0.1"]]
 
                     :source-paths ["dev"]
