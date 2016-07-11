dbf46c60b6d22ece5affc71d2152b2938b6c3be7
Henrik Mohr <luposlip@gmail.com>
2016-07-11 20:58:42 +0200

    Upgrade sente -> 1.9.0, clojure -> 1.8.0, cljs -> 1.9.93, core-async -> 0.2.385, component -> 0.3.1, environ -> 1.0.3, logback-classic -> 1.1.7, ring-core -> 1.5.0, ring-defaults -> 0.2.1, compojure -> 1.5.1, transit-cljs -> 0.8.239, bootstrap -> 4.0.0-alpha and figwheel -> 0.5.4-7

--- project.clj
+++ project.clj
@@ -4,25 +4,25 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.5.3"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189"]
-                 [org.clojure/core.async "0.2.374"]
-                 [com.stuartsierra/component "0.3.0"]
-                 [environ "1.0.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.93"]
+                 [org.clojure/core.async "0.2.385"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [environ "1.0.3"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
                  [org.clojure/tools.logging "0.3.1"]
 
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
-                 [compojure "1.4.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [compojure "1.5.1"]
                  [http-kit "2.1.19"]
 
-                 [com.taoensso/sente "1.6.0"]
+                 [com.taoensso/sente "1.9.0"]
                  [com.cognitect/transit-clj "0.8.285"]
-                 [com.cognitect/transit-cljs "0.8.232"]
+                 [com.cognitect/transit-cljs "0.8.239"]
 
-                 [reagent "0.5.1"]
-                 [org.webjars/bootstrap "3.3.6"]]
+                 [reagent "0.6.0-RC"]
+                 [org.webjars/bootstrap "4.0.0-alpha"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]]
 
@@ -43,9 +43,9 @@
 
              :dev [:dev-config
                    {:dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                   [figwheel "0.5.0-2"]]
+                                   [figwheel "0.5.4-7"]]
 
-                    :plugins [[lein-figwheel "0.5.0-2"]
+                    :plugins [[lein-figwheel "0.5.4-7"]
                               [lein-environ "1.0.1"]]
 
                     :source-paths ["dev"]
@@ -60,7 +60,7 @@
                                 :source-map true}}}}
 
                     :figwheel {:http-server-root "public"
-                               :port 3449
+                               :server-port 3449
                                :repl false
                                :css-dirs ["resources/public/css"]}}]
 
