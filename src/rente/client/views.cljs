38fd327adb002c4c86d3fe6e399cef9250b777ad
Cameron Desautels <camdez@gmail.com>
2017-07-26 22:22:14 +0800

    Fix small English usage issue

--- src/rente/client/views.cljs
+++ src/rente/client/views.cljs
@@ -6,9 +6,8 @@
    [:h1 (:title @data)]
    [:div "Hello world! This is reagent speaking!"]
    [:br]
-   [:div "Look in your browsers developer console to see the web socket communication when clicking below buttons."]
+   [:div "Look in your browser's developer console to see the web socket communication when clicking below buttons."]
    [:br]
    [:button {:on-click socket/test-socket-callback} "Send Message Callback"]
    [:br]
    [:button {:on-click socket/test-socket-event} "Send Message Event"]])
-
