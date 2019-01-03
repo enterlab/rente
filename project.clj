9408256a7af31254cfdba7bc6f92ecdc6c3656d8
Henrik Mohr <luposlip@gmail.com>
2019-01-03 21:29:06 +0100

    Upgraded clj->1.10.0, cljs->1.10.449, sente->1.13.1, reagent->0.8.1
    And:
    min-lein-version->2.8.3
    figwheel->0.5.18
    core.async->0.4.490
    component->0.4.0
    environ->1.1.0
    logback-classic->1.2.3
    tools.logging->0.4.1
    ring-core->1.7.1
    ring-defaults->0.3.2
    compojure->1.6.1
    http-kit->2.4.0-alpha2
    transit-clj->0.8.313
    transit-cljs->0.8.256
    bootstrap->4.2.1
    tools.namespace->0.3.0-alpha4
    
    TODO: Figure out which deps to remove

--- project.clj
+++ project.clj
@@ -1,30 +1,30 @@
-(defproject rente "1.0.0"
+(defproject rente "1.1"
   :description "A barebones Reagent+Sente app deployable to Heroku. Uses Figwheel (for development build) and Component (lifecycle management)."
   :url "http://enterlab.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.5.3"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.93"]
-                 [org.clojure/core.async "0.2.385"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [environ "1.0.3"]
-                 [ch.qos.logback/logback-classic "1.1.7"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :min-lein-version "2.8.3"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/core.async "0.4.490"]
+                 [com.stuartsierra/component "0.4.0"]
+                 [environ "1.1.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [org.clojure/tools.logging "0.4.1"]
 
-                 [ring/ring-core "1.5.0"]
-                 [ring/ring-defaults "0.2.1"]
-                 [compojure "1.5.1"]
-                 [http-kit "2.1.19"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.4.0-alpha2"]
 
-                 [com.taoensso/sente "1.9.0"]
-                 [com.cognitect/transit-clj "0.8.285"]
-                 [com.cognitect/transit-cljs "0.8.239"]
+                 [com.taoensso/sente "1.13.1"]
+                 [com.cognitect/transit-clj "0.8.313"]
+                 [com.cognitect/transit-cljs "0.8.256"]
 
-                 [reagent "0.6.0-rc"]
-                 [org.webjars/bootstrap "4.0.0-alpha"]]
+                 [reagent "0.8.1"]
+                 [org.webjars/bootstrap "4.2.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.1"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
 
   :source-paths ["src"]
   :resource-paths ["resources" "resources-index/prod"]
@@ -42,11 +42,11 @@
   :profiles {:dev-config {}
 
              :dev [:dev-config
-                   {:dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                   [figwheel "0.5.4-7"]]
+                   {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]
+                                   [figwheel "0.5.18"]]
 
-                    :plugins [[lein-figwheel "0.5.4-7"]
-                              [lein-environ "1.0.1"]]
+                    :plugins [[lein-figwheel "0.5.18"]
+                              [lein-environ "1.1.0"]]
 
                     :source-paths ["dev"]
                     :resource-paths ^:replace
